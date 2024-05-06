af99ba613eaa5817ea0a2799c63697a191007cab
Gaurang <gaurangjagtap@gmail.com>
2024-05-06 06:33:18 +0100

    json-server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults({
   static: "./build",
 });
@@ -15,5 +15,6 @@ server.use(
 
 server.use(router);
 server.listen(port, () => {
+  // eslint-disable-next-line no-template-curly-in-string
   console.log("Server is running on ${port}");
 });
